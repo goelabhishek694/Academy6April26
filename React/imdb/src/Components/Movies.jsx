9986023c9bf3ddd2e4bf345ff13dd3e5e8c79c06
Abhishek Goel <abhishekgoel@fynd.com>
2026-07-03 08:43:45 +0530

    Movies completed

--- React/imdb/src/Components/Movies.jsx
+++ React/imdb/src/Components/Movies.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Movies() {
+  const [movies, setMovies] = useState([
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 1",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 2",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 3",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 4",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 5",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 6",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 7",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 8",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 9",
+    },
+    {
+      url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+      title: "Movie 10",
+    },
+  ]);
+  const [pageNo, setPageNo] = useState(0);
+
+  const handlePrevPage = () => {
+    if (pageNo > 0) {
+      setPageNo(pageNo - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    setPageNo(pageNo + 1);
+  };
+
   return (
     <div>
-      
+      <div className="text-6xl font-bold text-center m-4">Movies</div>
+      <div className="flex justify-evenly gap-8 flex-wrap">
+        {movies.map((movieObj) => (
+          <div
+            className="h-[40vh] w-[200px] bg-cover bg-center rounded-lg hover:scale-110 duration-300 hover:cursor-pointer flex flex-col justify-end "
+            style={{ backgroundImage: `url(${movieObj.url})` }}
+          >
+            <div className="text-white w-full text-center p-2 bg-gray-900/40 rounded-lg">
+              {movieObj.title}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="bg-gray-400 h-[50px] p-4 mt-8 w-full flex justify-center items-center gap-2 text-xl">
+        <div onClick={handlePrevPage} className="px-8">
+          <i class="fa-solid fa-arrow-left"></i>
+        </div>
+        <div>{pageNo}</div>
+        <div onClick={handleNextPage} className=" px-8 ">
+          <i class="fa-solid fa-arrow-right"></i>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
