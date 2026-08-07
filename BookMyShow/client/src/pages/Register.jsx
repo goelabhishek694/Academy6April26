e038c9efb46d6aa9d9d258291a5a5e703cee53e6
Abhishek Goel <abhishekgoel@fynd.com>
2026-08-07 08:39:04 +0530

    register page

--- BookMyShow/client/src/pages/Register.jsx
+++ BookMyShow/client/src/pages/Register.jsx
@@ -1,12 +1,67 @@
 import React from 'react'
+import {Button, Form, Input} from 'antd'
 import { Link } from 'react-router-dom'
 
 function Register() {
+    const onFinish = (values) => {
+        console.log(values)
+    }
+
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to='/login'>Go to Login</Link>
-    </div>
+    <>
+    <main className='App-header'>
+        <h1>Register On BookMyShow</h1>
+        <section className="mw-500 text-center px-3">
+            <Form layout="vertical" onFinish={onFinish}>
+            <Form.Item
+                    label="Name"
+                    htmlFor="name"
+                    name="name"
+                    className='d-block'
+                    rules={[
+                        {required: true, message: "Name is required"},
+                    ]}
+                >
+                    <Input id="name" type="text" placeholder="Enter your Name"/>
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    htmlFor="email"
+                    name="email"
+                    className='d-block'
+                    rules={[
+                        {required: true, message: "Email is required"},
+                        {type:"email", message: "Please enter a valid email"}
+                    ]}
+                >
+                    <Input id="email" type="text" placeholder="Enter your Email"/>
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    htmlFor="password"
+                    name="password"
+                    className='d-block'
+                    rules={[
+                        {required: true, message: "Password is required"}
+                    ]}
+                >
+                    <Input id="password" type="password" placeholder="Enter your Password"/>
+                </Form.Item>
+
+                <Form.Item
+                    className='d-block'
+                >
+                    <Button type="primary" block htmlType="submit" style={{fontSize:"1rem", fontWeight:"600"}}>Register</Button>
+                </Form.Item>
+            </Form>
+            <div>
+                <p>Existing User ? <Link to="/login">Login Here</Link></p>
+            </div>
+        </section>
+    </main>
+    </>
   )
 }
 
